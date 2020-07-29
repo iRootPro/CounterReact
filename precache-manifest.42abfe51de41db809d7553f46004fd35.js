92737097b58c1fb844fa9ce78c048138992acac6
Alexander Neupokoev <admin@armavir.ru>
2020-07-29 11:26:00 +0300

    Updates

---
+++ precache-manifest.42abfe51de41db809d7553f46004fd35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ceff389226c0665528a2b037d00afb",
+    "url": "/CounterReact/index.html"
+  },
+  {
+    "revision": "7705cc2ea71be113710d",
+    "url": "/CounterReact/static/css/main.59d70f3d.chunk.css"
+  },
+  {
+    "revision": "f3dd895a156adfc6dfbe",
+    "url": "/CounterReact/static/js/2.e0c1b560.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CounterReact/static/js/2.e0c1b560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7705cc2ea71be113710d",
+    "url": "/CounterReact/static/js/main.db02b29b.chunk.js"
+  },
+  {
+    "revision": "5bad266f221ddb5fefee",
+    "url": "/CounterReact/static/js/runtime-main.8320c4c3.js"
+  }
+]);
